33f36bbd1fad2b3aa943fb7618745a1d2ed585d3
Saúl Caballero Mondragón <224971101+saul-caballero@users.noreply.github.com>
2026-01-12 23:50:41 -0600

    refactor: migrate MVV component to BEM structure
    - Refactored MVV component class names to BEM
    - Improved button state handling
    - Unified styles and JS selectors
    - No behavior changes, structure only

---
+++ component-mvv.js
@@ -0,0 +1,31 @@
+// Variables
+const mvvButton = document.querySelector('.mvv__buttons');
+const infoMvv = document.querySelector('#mvv__info')
+
+// Eventos
+mvvButton.addEventListener('click', cambiarInfoMvv);
+
+// Funciones
+function cambiarInfoMvv(e){
+    e.preventDefault();
+    if(!e.target.classList.contains('mvv__button')){
+        return;
+    }
+    const btnActivo = document.querySelector('.mvv__button--active');
+    if(btnActivo){
+         btnActivo.classList.remove('mvv__button--active');
+    }
+    e.target.classList.add('mvv__button--active');
+    
+
+    const idBoton = e.target.id;
+    const secciones = infoMvv.querySelectorAll('.mvv__info');
+
+    secciones.forEach(seccion => {
+        seccion.classList.remove('mvv__text--active');
+
+        if(seccion.getAttribute('data-id') === idBoton){
+            seccion.classList.add('mvv__text--active');
+        }
+    });
+}
