bb52037449545f38b063bdbdf63f851e80501979
Saul Caballero <224971101+saul-caballero@users.noreply.github.com>
2026-01-13 11:21:48 -0600

    docs: Agregar comentarios explicativos al componente MVV

--- component-mvv.js
+++ component-mvv.js
@@ -1,6 +1,6 @@
 // Variables
 const mvvButton = document.querySelector('.mvv__buttons');
-const infoMvv = document.querySelector('#mvv__info')
+const infoMvv = document.querySelector('#mvv__info');
 
 // Eventos
 mvvButton.addEventListener('click', cambiarInfoMvv);
@@ -8,24 +8,29 @@ mvvButton.addEventListener('click', cambiarInfoMvv);
 // Funciones
 function cambiarInfoMvv(e){
     e.preventDefault();
+    // Si el objetivo es diferente a mvv__button...
     if(!e.target.classList.contains('mvv__button')){
-        return;
+        return; // pausar o dejar de ejecutar
     }
+    // Variable de existencia
     const btnActivo = document.querySelector('.mvv__button--active');
+    // Si existe..
     if(btnActivo){
-         btnActivo.classList.remove('mvv__button--active');
+         btnActivo.classList.remove('mvv__button--active'); // borrar
     }
+    // Aplicar activacion al objetivo clickeado
     e.target.classList.add('mvv__button--active');
     
-
+    // Extraer id de boton
     const idBoton = e.target.id;
+    // Crear array nodelist para recorrer que texto mostrar
     const secciones = infoMvv.querySelectorAll('.mvv__text');
-
     secciones.forEach(seccion => {
+        // Remover primero todo display block (desaparecer todos)
         seccion.classList.remove('mvv__text--active');
-
+        // Obetner data-id y Si el id que se clickeo es igual hacer...
         if(seccion.getAttribute('data-id') === idBoton){
-            seccion.classList.add('mvv__text--active');
+            seccion.classList.add('mvv__text--active'); // Agregar block para mostrar info
         }
     });
 }
